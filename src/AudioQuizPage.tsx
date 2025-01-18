96552abb07f1fa7e0d0a084ac58b8c2c7daf80a9
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2025-01-18 11:20:41 -0700

    added select menu so it is easier

--- src/AudioQuizPage.tsx
+++ src/AudioQuizPage.tsx
@@ -2,18 +2,17 @@ import React, { useState } from "react";
 import { GURMUKHI_LETTERS } from "./utils/constants.ts";
 
 function AudioQuizPage() {
-  const [currentLetter, setCurrentLetter] = useState(getRandomLetter());
   const [message, setMessage] = useState("");
   const [showModal, setShowModal] = useState(false);
   const [isCorrect, setIsCorrect] = useState(false);
   const [scoreObj, setScoreObj] = useState({ correct: 0, incorrect: 0 });
   const audioRef = React.useRef<HTMLAudioElement | null>(null);
-
-  function getRandomLetter() {
-    return GURMUKHI_LETTERS[
-      Math.floor(Math.random() * GURMUKHI_LETTERS.length)
-    ];
-  }
+  const [lastLetterIndexToShow, setLastLetterIndexToShow] = useState(
+    GURMUKHI_LETTERS.length,
+  );
+  const [currentLetter, setCurrentLetter] = useState(
+    getRandomLetter(lastLetterIndexToShow),
+  );
 
   function checkAnswer(selectedLetter) {
     if (selectedLetter === currentLetter.letter) {
@@ -26,10 +25,14 @@ function AudioQuizPage() {
     setShowModal(true);
   }
 
-  function nextQuestion() {
+  function nextQuestion(newLastIndex: number | null) {
     setMessage("");
     setShowModal(false);
-    setCurrentLetter(getRandomLetter());
+    if (newLastIndex) {
+      setCurrentLetter(getRandomLetter(newLastIndex));
+    } else {
+      setCurrentLetter(getRandomLetter(lastLetterIndexToShow));
+    }
   }
 
   function showAnswer() {
@@ -41,12 +44,7 @@ function AudioQuizPage() {
       <h2 className="text-2xl font-bold text-center mb-6 text-gray-900 dark:text-gray-50">
         Audio Quiz
       </h2>
-      <audio
-        src={currentLetter.audioSrc}
-        controls
-        autoPlay
-        ref={audioRef}
-      />
+      <audio src={currentLetter.audioSrc} controls autoPlay ref={audioRef} />
       <details className="mt-4 text-lg text-gray-700 dark:text-gray-300">
         <summary>Transliteration</summary>
         <p>{currentLetter.transliteration}</p>
@@ -54,12 +52,32 @@ function AudioQuizPage() {
       <div>
         Correct: {scoreObj.correct} Incorrect: {scoreObj.incorrect}
       </div>
+
+      <p>{lastLetterIndexToShow}</p>
+      <select
+        value={lastLetterIndexToShow}
+        onChange={(e) => {
+          const newLastIndex = parseInt(e.target.value);
+          setLastLetterIndexToShow(newLastIndex);
+          nextQuestion(newLastIndex);
+        }}
+      >
+        <option value={5}>ੳ ਅ ੲ ਸ ਹ</option>
+        <option value={10}>ਕ ਖ ਗ ਘ ਙ</option>
+        <option value={15}>ਚ ਛ ਜ ਝ ਞ</option>
+        <option value={20}>ਟ ਠ ਡ ਢ ਣ</option>
+        <option value={25}>ਤ ਥ ਦ ਧ ਨ</option>
+        <option value={30}>ਪ ਫ ਬ ਭ ਮ</option>
+        <option value={35}>ਯ ਰ ਲ ਵ ੜ </option>
+      </select>
+
       <div className="grid grid-cols-5 gap-4 pt-6">
         {GURMUKHI_LETTERS.map((letter, index) => (
           <button
             key={index}
             onClick={() => checkAnswer(letter.letter)}
-            className="p-4 bg-blue-600 text-white rounded shadow-md hover:bg-blue-700"
+            className="p-4 bg-blue-600 text-white rounded shadow-md hover:bg-blue-700 disabled:bg-gray-300 disabled:text-gray-500"
+            disabled={index >= lastLetterIndexToShow}
           >
             {letter.letter}
           </button>
@@ -152,6 +170,12 @@ function Modal({
       </div>
     </div>
   );
+}
+
+function getRandomLetter(lastLetterIndexToShow: number) {
+  const a = GURMUKHI_LETTERS[Math.floor(Math.random() * lastLetterIndexToShow)];
+  console.log(a, lastLetterIndexToShow);
+  return a;
 }
 
 export default AudioQuizPage;
